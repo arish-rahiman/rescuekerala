0b08105713b9438838ccc79fd77d539e81aa4e9a
Irshad Melethil <irshadmelethil@gmail.com>
2018-08-20 00:00:53 +0530

    added Volunteer, Center and InventoryItem admin

--- supplies_tracker/admin.py
+++ supplies_tracker/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import InventoryItem, Volunteer, Center, InventoryItemStock
+
+
+class BaseAdmin(admin.ModelAdmin):
+    exclude = ('created_at', 'updated_at')
+
+class VolunteerAdmin(BaseAdmin):
+    list_filter = ('center',)
+    list_display = ('get_name', 'phone_number', 'center')
+
+    def get_name(self, obj):
+        return obj.user.get_full_name()
+
+
+class CenterAdmin(BaseAdmin):
+    list_filter = ('district', 'status', 'center_type', 'notes')
+    list_display = ('name', 'district')
+
+
+class InventoryItemAdmin(BaseAdmin):
+    list_filter = ('unit', 'category')
+    list_display = ('name', 'unit', 'category')
+
+
+admin.site.register(Volunteer, VolunteerAdmin)
+admin.site.register(Center, CenterAdmin)
+admin.site.register(InventoryItem, InventoryItemAdmin)
